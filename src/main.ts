e6736a83a2e2b8c98d36dffac5c993d40e4cbbd0
mariana soares <marianasoares1240@gmail.com>
2025-05-27 20:30:59 +0000

    feat: solução d tarefa

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,24 @@
-// Escreva seu código aqui!
+// number
+let numeroDeArCondicionados: number = 2;
+
+// string
+let nomeDaProfessora: string = "Lana";
+
+// boolean
+let luzEstaAcesa: boolean = true;
+
+// undefined
+let quemEstaNoCorredor: undefined = undefined;
+
+// objeto
+let lousa: { cor: string; temCoisasEscritas: boolean } = {
+  cor: "branca",
+  temCoisasEscritas: true
+};
+
+
+console.log("Número de ar condicionados:", numeroDeArCondicionados);
+console.log("Nome da professora:", nomeDaProfessora);
+console.log("A luz está acesa?", luzEstaAcesa);
+console.log("Quem está no corredor:", quemEstaNoCorredor);
+console.log("Lousa:", lousa);
